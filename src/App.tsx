70c523a0512fb00dd0f9d65fc8f773c7e7666ae0
Ben Allen <bta73693@gmail.com>
2024-11-28 11:13:02 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css'
+import Scoresheet from './components/Scoresheet'
+
+function App() {
+  return <Scoresheet />
+}
+
+export default App
---
+++ src/components/Scoresheet.tsx
@@ -0,0 +1,24 @@
+import './Scoresheet.css'
+
+const Scoresheet = () => {
+  return (
+    <table>
+      <tr>
+        <th>We</th>
+        <th>They</th>
+      </tr>
+      <tr className="break"></tr>
+      <tr>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr className="break"></tr>
+      <tr>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  )
+}
+
+export default Scoresheet
